483de452604c56060a54a114c9c756372c663b03
FmodouX <frederic@fmodoux.biz>
2020-02-15 10:09:02 +0100

    Create keyLight.py

---
+++ keyLight.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+elGatoIp = "x.x.x.x"
+
+lightSetup = {
+    "on" : 1, # '0' for off, '1' for on
+    "brightness" : 50, # from 2 to 100 (%)
+    "temperature" : 178 # notYetIdentified the relation between °kelvin and the passed value XD !!! from 143 (7000°K) to 343 (2900°K)
+}
+
+if (lightSetup.get("on", 0) == 0):
+    bodyContent = '{"numberOfLights": 1,"lights": [{"on": 0}]}'
+else:
+     bodyContent = '{"numberOfLights": 1,"lights": [' + json.dumps(lightSetup) + ']}'
+
+response = requests.put("http://{}:9123/elgato/lights".format(elGatoIp), bodyContent)
